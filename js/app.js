14a222aa22300c7104af2ab5d3cffac6c397414f
MykolaRudenko7 <m.rudenko.rb@gmail.com>
2022-06-29 13:20:46 +0300

    first comit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const burger = document.querySelector(".header__burger");
+    const header = burger.closest(".header");
+    if (burger) burger.addEventListener("click", (function(e) {
+        if (header) header.classList.toggle("open-burger");
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
